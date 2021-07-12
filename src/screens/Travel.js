89fa5d5188ef81715dad3a870a78a7472f2426a8
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2021-07-12 09:57:09 +0000

    add form logic

--- src/screens/Travel.js
+++ src/screens/Travel.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import styles from "../styles/travel.module.css";
 
 const Travel = () => {
+  const [travel, setTravel] = useState(false);
   return (
-    <div>
-      travel
-    </div>
-  )
-}
+    <div className={styles.travel}>
+      <Form>
+        Any recent travel?
+        <br />
+        <div className="d-inline-flex justify-content-center align-items-center ">
+          <>
+            <label htmlFor="yes" className="py-2 mb-1 mr-2">
+              Yes
+            </label>
+            <Form.Check
+              id="yes"
+              type="radio"
+              name="travel"
+              className="mr-2"
+              value={travel}
+              onChange={() => setTravel(true)}
+            />
+          </>
+          <>
+            <label htmlFor="no" className="py-2 mb-1 mr-2">
+              No
+            </label>
+            <Form.Check
+              type="radio"
+              name="travel"
+              value="no"
+              onChange={() => setTravel(false)}
+            />
+          </>
+        </div>
+      </Form>
 
-export default Travel
+      {travel && <h1>Hello</h1>}
+    </div>
+  );
+};
+
+export default Travel;
