38dac047bcc605d652b4b37ee7baa8516c8bbbff
Dchole <derekoware47@gmail.com>
2021-07-21 21:15:23 +0000

    testing puppeteer

--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,7 +1,11 @@
 import puppeteer from "puppeteer"
 
 export const getGoogleMap = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    ignoreDefaultArgs: ["--disable-extensions"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false
+  })
   const page = await browser.newPage()
   await page.goto("https://maps.google.com", { waitUntil: "networkidle2" })
   await page.screenshot({ path: "../images/google-map.png" })
